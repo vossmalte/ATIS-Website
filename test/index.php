fe5e767d0f6b845edac871f71f6c50544c0e1d20
Malte Voss <s_voss@i08fs1.atis-stud.uni-karlsruhe.de>
2016-11-11 14:12:11 +0100

    First commit: Bestehende Daten aufgenommen.

---
+++ test/index.php
@@ -0,0 +1,20 @@
+<html>
+<title>Maltes Notizen</title>
+<form>
+ Save this:
+ <input type="text" name="save"><br>
+</form>
+</html>
+<?php
+$dir = '.';
+echo "<h1>Inhalte dieses Verzeichnisses:</h1>";
+$inhalte = scandir($dir);
+asort($inhalte);
+
+echo "<ul>";
+foreach ($inhalte as $i) {
+  if (!(substr($i,0,1) == "."))
+  echo "<a href='".$i."'><li>".$i."</li></a>";
+}
+echo "<ul>";
+ ?>
