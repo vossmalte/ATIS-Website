36c79bd6aa6cc38d796bcad0b5e9cd6a97a0a61f
malte <malte.voss@ira.uka.de>
2017-01-28 14:28:11 +0100

    Verlinkungen zu Unterseiten, nun .php

--- index.php
+++ index.php
@@ -1,14 +1,29 @@
 <HTML>
 <body>
-
-<title>Malte Vo&szlig; - KIT</title>
-<h1>Informatik am KIT</h1>
+<head>
+    <title>Malte Vo&szlig; - Home</title>
+    <link rel="stylesheet" type="text/css" href="freestyler.css">
+</head>
+<h1>Willkommen!</h1>
 <img src="malte.jpg" alt="Portrait" width="10%"/>
 
+<h2>Studium</h2>
 <p>
   Seit Oktober 2015 studiere ich Informatik am KIT. <br>
   Im Wintersemester 2016/17 werde ich <a href="tut">Tutor</a>
      für die <a href="https://sdqweb.ipd.kit.edu/wiki/Vorlesung_Programmieren_WS16/17"> Programmieren-Vorlesung</a> sein.
 </p>
+
+<h2>Sonstiges</h2>
+<p>
+    <ul>
+        <li><a href="ultimate">Ultimate</a></li>
+        <li><a href="atis">ATIS - meine Tipps und Tricks</a></li>
+        <li><a href="test">Test area</a>, user=user,pw=2389979753^(sqrt(7+2)^(-1)</li>
+    </ul>
+</p>
+<?php
+    include("footer.html");
+?>
 </body>
 </HTML>
